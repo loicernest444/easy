d66b2eb78e4855224b7697114420e86c59f65889
loicernest444 <loicernest444@gmail.com>
2022-12-07 09:49:29 +0100

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="<?php echo get_phrase('the_best_software_to_run_your_school'); ?>" name="description" />
     <meta content="Coderthemes" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="<?php echo $this->settings_model->get_favicon(); ?>">
